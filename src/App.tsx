e54debc9d46daf241c2dc3778cd7793af66ee338
Baqytbayev <bakitbaev98@gmail.com>
2024-08-28 16:56:08 +0500

    Вывел Ñна страницу

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,35 @@ import React from 'react';
 import './App.css';
 import {useQuery} from "react-query";
 import {GetUsers} from "./api/getUsers";
+import {
+  Avatar,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow
+} from "@mui/material";
+
+interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  phone: string;
+  birthDate: string;
+  image: string;
+}
+
+interface Data {
+  users: User[];
+  total: number;
+  limit: number;
+  skip: number;
+}
 
 const App = () => {
-  const {data, isLoading, error} = useQuery([
+  const {data, isLoading, error} = useQuery<Data>([
     'getUsers', {limit: 10}
   ], () => GetUsers({limit: 10}))
   console.log(data)
@@ -16,9 +42,30 @@ const App = () => {
     return <div>Error loading popular products</div>;
   }
   return (
-    <div className="App">
-      Hello
-    </div>
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Фото</TableCell>
+            <TableCell>Имя</TableCell>
+            <TableCell>Телефон</TableCell>
+            <TableCell>Дата рождения</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data?.users.map((user: any) => (
+            <TableRow key={user.id}>
+              <TableCell>
+                <Avatar src={user.image} alt={`${user.firstName} ${user.lastName}`} />
+              </TableCell>
+              <TableCell>{`${user.firstName} ${user.lastName}`}</TableCell>
+              <TableCell>{user.phone}</TableCell>
+              <TableCell>{new Date(user.birthDate).toLocaleDateString()}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
 
